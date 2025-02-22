03cd83b25aa9a90baa6b18eacdbbf9da7c0d3669
Ashiqur Rahman <imashiqe@gmail.com>
2025-02-22 23:26:49 +0600

    get data from db

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  const result = await prisma.post.create({
-    data: {
-      title: "Hello World Now",
-      content: "This is a test post",
-      authorName: "Alice",
-      published: false,
-    },
-  });
-
-  console.log(result);
+  //   const result = await prisma.post.create({
+  //     data: {
+  //       title: "Hello World Now",
+  //       content: "This is a test post",
+  //       authorName: "Alice",
+  //       published: false,
+  //     },
+  //   });
+  //   console.log(result);
+  const getAllFromDB = await prisma.post.findMany();
+  console.log(getAllFromDB);
 };
 
 main();
